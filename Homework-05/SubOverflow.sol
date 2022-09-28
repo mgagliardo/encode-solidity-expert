d837ce9d5231d514a75ca98577d1a87031092266
Miguel Gagliardo <migag9@gmail.com>
2022-09-28 13:16:56 +0100

    Fixing SubOverFlow

--- Homework-05/SubOverflow.sol
+++ Homework-05/SubOverflow.sol
@@ -8,14 +8,9 @@ contract SubOverflow {
     function subtract(uint256 x, uint256 y) public pure returns (uint256) {
         // Write assembly code that handles overflows
         assembly {
-            let z := sub(x, y)
-            let k := add(x, y)
-            switch lt(z, k)
-            case  true {
-                mstore(0x80, z)
-            }
-            default {
-                mstore(0x80, 0)
+            mstore(0x80, 0)
+            if gt(x, y) {
+                mstore(0x80, sub(x, y))
             }
             return(0x80, 32)
         }
